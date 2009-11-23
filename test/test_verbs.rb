37d833053e45cfe98a6616ae6bfdf9c67596ce43
Andy Rossmeissl <andy@rossmeissl.net>
2009-11-23 16:39:43 -0500

    Start testing

--- test/test_verbs.rb
+++ test/test_verbs.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestVerbs < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_conjugation
+    assert_equal :break, Verbs::Conjugator.conjugate(:break, :tense => :present, :person => :first, :plurality => :singular) 
   end
 end
