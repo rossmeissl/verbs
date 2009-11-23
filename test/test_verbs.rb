2a4b20a47ca8a53bc82bb7569caeea917dcc40d1
Andy Rossmeissl <andy@rossmeissl.net>
2009-11-23 16:50:28 -0500

    Test verbosity

--- test/test_verbs.rb
+++ test/test_verbs.rb
@@ -1,7 +1,10 @@
 require 'helper'
 
 class TestVerbs < Test::Unit::TestCase
-  def test_conjugation
+  def test_copular_conjugation
+    assert_equal :am, Verbs::Conjugator.conjugate(:be, :tense => :present, :person => :first, :plurality => :singular)
+  end
+  def test_irregular_conjugation 
     assert_equal :break, Verbs::Conjugator.conjugate(:break, :tense => :present, :person => :first, :plurality => :singular) 
   end
 end
