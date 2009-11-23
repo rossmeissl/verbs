39c2c58c0d78e64c3b1f8804265e9cc539269c83
Andy Rossmeissl <andy@rossmeissl.net>
2009-11-23 16:53:02 -0500

    Test present forms of the copular verb

--- test/test_verbs.rb
+++ test/test_verbs.rb
@@ -3,6 +3,10 @@ require 'helper'
 class TestVerbs < Test::Unit::TestCase
   def test_copular_conjugation
     assert_equal :am, Verbs::Conjugator.conjugate(:be, :tense => :present, :person => :first, :plurality => :singular)
+    assert_equal :are, Verbs::Conjugator.conjugate(:be, :tense => :present, :person => :second, :plurality => :singular)
+    assert_equal :is, Verbs::Conjugator.conjugate(:be, :tense => :present, :person => :third, :plurality => :singular)
+    assert_equal :are, Verbs::Conjugator.conjugate(:be, :tense => :present, :person => :first, :plurality => :plural)
+    assert_equal :are, Verbs::Conjugator.conjugate(:be, :tense => :present, :person => :third, :plurality => :plural)
   end
   def test_irregular_conjugation 
     assert_equal :break, Verbs::Conjugator.conjugate(:break, :tense => :present, :person => :first, :plurality => :singular) 
